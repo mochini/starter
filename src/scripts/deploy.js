014cf7dba532bfe1665d5538d71cc7b4ed39a077
greg@thinktopography.com <greg@thinktopography.com>
2019-03-20 13:55:45 -0400

    dont checkin repo

--- src/scripts/deploy.js
+++ src/scripts/deploy.js
@@ -1,8 +1,12 @@
 import '../server/lib/environment'
 import shipitfile from '../deploy/shipitfile'
 import Shipit from 'shipit-cli'
+import mkdirp from 'mkdirp'
+import path from 'path'
 
 const deploy = async () => {
+
+  mkdirp.sync(path.join('repo'))
 
   const shipit = new Shipit({ environment: 'production' })
 
